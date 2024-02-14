df3c1c0e95a0964dcc60b67aac68909ce54db6bc
Evgeniy Moskatov <evgeniy.moskatov@gmail.com>
2024-02-14 23:53:11 +0300

    add rate Lectors in class Student

--- main.py
+++ main.py
@@ -7,6 +7,16 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_lecturer(self, lecturer, course, grade):
+        if isinstance(lecturer,
+                      Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
 
 class Mentor:
     def __init__(self, name, surname):
@@ -16,7 +26,10 @@ class Mentor:
 
 
 class Lecturer(Mentor):
-    pass
+    def __init__(self, name, surname):
+        # наследуем функциональность конструктора из класса-родителя
+        super().__init__(name, surname)
+        self.grades = {}
 
 
 class Reviewer(Mentor):
@@ -33,11 +46,18 @@ class Reviewer(Mentor):
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
 
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
+cool_rewiewer = Reviewer('Some', 'Buddy')
+cool_rewiewer.courses_attached += ['Python']
 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_rewiewer.rate_hw(best_student, 'Python', 10)
+cool_rewiewer.rate_hw(best_student, 'Python', 10)
+cool_rewiewer.rate_hw(best_student, 'Python', 10)
 
 print(best_student.grades)
+
+cool_lector = Lecturer('Some', 'Buddy')
+cool_lector.courses_attached += ['Python']
+
+best_student.rate_lecturer(cool_lector, 'Python', 10)
+
+print(cool_lector.grades)
