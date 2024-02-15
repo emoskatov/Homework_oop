e65b1e03840027e12aea0ff20d87a4ee82ff9e42
Evgeniy Moskatov <evgeniy.moskatov@gmail.com>
2024-02-16 01:07:16 +0300

    Adding comparison methods and creating class instances

--- main.py
+++ main.py
@@ -14,9 +14,26 @@ class Student:
                f"Курсы в процессе изучения: {', '.join(self.courses_in_progress)} \n" \
                f"Завершенные курсы: {', '.join(self.finished_courses)}"
 
+    def __gt__(self, student):
+        if isinstance(student, Student):
+            return self.avg_rate() > student.avg_rate()
+        else:
+            return f"{student.name} не является объектом класса Student"
+
+    def __lt__(self, student):
+        if isinstance(student, Student):
+            return self.avg_rate() < student.avg_rate()
+        else:
+            return f"{student.name} не является объектом класса Student"
+
+    def __eq__(self, student):
+        if isinstance(student, Student):
+            return self.avg_rate() == student.avg_rate()
+        else:
+            return f"{student.name} не является объектом класса Student"
+
     def avg_rate(self):
         grades_list = sum(self.grades.values(), [])
-        print(grades_list)
         return sum(grades_list) / len(grades_list)
 
     def rate_lecturer(self, lecturer, course, grade):
@@ -46,9 +63,26 @@ class Lecturer(Mentor):
     def __str__(self):
         return f"Имя: {self.name} \nФамилия: {self.surname} \nСредняя оценка за лекции: {self.avg_rate()}"
 
+    def __gt__(self, lecturer):
+        if isinstance(lecturer, Lecturer):
+            return self.avg_rate() > lecturer.avg_rate()
+        else:
+            return f"{lecturer.name} не является объектом класса Lecturer"
+
+    def __lt__(self, lecturer):
+        if isinstance(lecturer, Lecturer):
+            return self.avg_rate() < lecturer.avg_rate()
+        else:
+            return f"{lecturer.name} не является объектом класса Lecturer"
+
+    def __eq__(self, lecturer):
+        if isinstance(lecturer, Lecturer):
+            return self.avg_rate() == lecturer.avg_rate()
+        else:
+            return f"{lecturer.name} не является объектом класса Lecturer"
+
     def avg_rate(self):
         grades_list = sum(self.grades.values(), [])
-        print(grades_list)
         return sum(grades_list) / len(grades_list)
 
 
@@ -66,25 +100,44 @@ class Reviewer(Mentor):
             return 'Ошибка'
 
 
-best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python', 'Java']
+student_ivanov_a = Student('Алексей', 'Иванов', 'Мужской')
+student_ivanov_a.courses_in_progress += ['Python', 'Java']
+student_sokolov_k = Student('Кирилл', 'Соколов', 'Мужской')
+student_sokolov_k.courses_in_progress += ['Python']
 
-cool_reviewer = Reviewer('Some', 'Buddy')
-cool_reviewer.courses_attached += ['Python']
+reviewer_morozov_p = Reviewer('Павел', 'Морозов')
+reviewer_morozov_p.courses_attached += ['Python']
+reviewer_stepanov_v = Reviewer('Вадим', 'Степанов')
+reviewer_stepanov_v.courses_attached += ['Python', 'Java']
 
-cool_reviewer.rate_hw(best_student, 'Python', 10)
-cool_reviewer.rate_hw(best_student, 'Python', 10)
-cool_reviewer.rate_hw(best_student, 'Python', 10)
+reviewer_morozov_p.rate_hw(student_sokolov_k, 'Python', 10)
+reviewer_stepanov_v.rate_hw(student_sokolov_k, 'Python', 7)
+reviewer_stepanov_v.rate_hw(student_ivanov_a, 'Java', 9)
+reviewer_stepanov_v.rate_hw(student_ivanov_a, 'Python', 9)
 
-print(best_student.grades)
+lector_morozov_p = Lecturer('Павел', 'Морозов')
+lector_morozov_p.courses_attached += ['Python']
+lector_popov_d = Lecturer('Данил', 'Попов')
+lector_popov_d.courses_attached += ['Python', 'Java']
 
-cool_lector = Lecturer('Some', 'Buddy')
-cool_lector.courses_attached += ['Python', 'Java']
+student_ivanov_a.rate_lecturer(lector_morozov_p, 'Python', 8)
+student_ivanov_a.rate_lecturer(lector_popov_d, 'Java', 10)
+student_ivanov_a.rate_lecturer(lector_popov_d, 'Python', 9)
 
-best_student.rate_lecturer(cool_lector, 'Python', 10)
-best_student.rate_lecturer(cool_lector, 'Python', 8)
-best_student.rate_lecturer(cool_lector, 'Java', 5)
+print(student_sokolov_k)
+print()
 
-print(cool_reviewer)
-print(cool_lector)
-print(best_student)
+print(reviewer_stepanov_v)
+print()
+
+print(lector_morozov_p)
+print()
+
+print(lector_morozov_p == lector_popov_d)
+print(lector_morozov_p < lector_popov_d)
+print(lector_morozov_p > lector_popov_d)
+print()
+
+print(student_sokolov_k == student_ivanov_a)
+print(student_sokolov_k < student_ivanov_a)
+print(student_sokolov_k > student_ivanov_a)
